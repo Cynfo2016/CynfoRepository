45fbc66b79b69ee3ab244901827e2bdbb1b61b93
Moises <moisesynfan@gmail.com>
2016-12-08 11:52:44 -0400

    My Second change

--- CynfoApp/Startup.cs
+++ CynfoApp/Startup.cs
@@ -9,7 +9,7 @@ namespace CynfoApp
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            System.Console.WriteLine("Hello");
+            System.Console.WriteLine("Hello bitch");
         }
     }
 }
