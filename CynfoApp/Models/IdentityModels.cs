8f2bcc09d6ceaacd667f01b4fdd11134c95c7ac6
Marianny Soranlli Torres Marte <torresmarte@hotmail.com>
2016-12-08 18:10:01 -0400

    Eliminación de Cambios Innecesarios

--- CynfoApp/Models/IdentityModels.cs
+++ CynfoApp/Models/IdentityModels.cs
@@ -7,31 +7,28 @@ using System;
 
 namespace CynfoApp.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser
-    // class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        internal Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager userManager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            throw new NotImplementedException();
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
         }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        static ApplicationDbContext()
-        {
-            Database.SetInitializer(new MySqlInitializer());
-        }
-
         public ApplicationDbContext()
-          : base("DefaultConnection")
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
-        internal static IDisposable Create()
+        public static ApplicationDbContext Create()
         {
-            throw new NotImplementedException();
+            return new ApplicationDbContext();
         }
     }
 }
