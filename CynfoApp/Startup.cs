c1dc00c364b8bbd4b7472f37581e5bdb09d2618b
Marianny Soranlli Torres Marte <torresmarte@hotmail.com>
2016-12-08 11:38:13 -0400

    Add project files.

---
+++ CynfoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CynfoApp.Startup))]
+namespace CynfoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
