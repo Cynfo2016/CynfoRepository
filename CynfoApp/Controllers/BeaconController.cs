6d8daa51fec311fb3c01898887246483956ab87d
Marianny Soranlli Torres Marte <torresmarte@hotmail.com>
2016-12-09 13:12:32 -0400

    BeaconClasses

---
+++ CynfoApp/Controllers/BeaconController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CynfoApp.Models;
+//using CynfoApp.azureUtils;
+
+namespace CynfoApp.Controllers
+{
+    public class BeaconController : Controller
+    {
+        // GET: Beacon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CynfoApp/Models/BeaconModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+
+
+namespace CynfoApp.Models
+{
+    [Table("cynfodb.beacon")]
+    public partial class BeaconModels
+    {
+        public int idBeacon { get; set; }
+
+        public string beaconAddress { get; set; }
+
+        public int beaconMajor { get; set; }
+
+        public int beaconMinor { get; set; }
+
+        public string Place_idPlace { get; set; }
+
+    }
+
+    public class DefaultConnectioncontext : DbContext
+    {
+        public DbSet<BeaconModels> beacon { get; set; }
+    }
+}
